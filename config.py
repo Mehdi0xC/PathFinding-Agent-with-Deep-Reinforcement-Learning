737fe85e7eedcc1083ea0d8b4eee5339b6a13f58
Mehdi <Mehdi0xC@gmail.com>
2019-10-17 14:24:06 +0330

    adding files

---
+++ config.py
@@ -0,0 +1,33 @@
+learningCoreSettings = {
+    "nNeurons" : 10,
+    "gamma" : 0.9,
+    "memoryCapacity" : 10000,
+    "learningRate" : 0.005,
+    "batchSize" : 25,
+    "activationFunction" :"relu",
+    "nOutputs" : 3,
+    "nInputs" : 3,
+    "reg" : 0,  
+    "backend" : "pytorch",
+    "rewardAmount" : 0.1,
+    "punishAmount" : -1,
+    "softmaxTemperature" : 10,
+    "learningIterations" : 1000,
+    "predictionIterations" : 1000,
+    "device" : "COM5"}
+
+learningDeviceSettings = {
+        "devicePortName" : "COM5",
+        "baudrate" : 115200
+    }
+
+environmentSettings = {
+    "sensorSize" : 15,
+    "agentWidth" : 96,
+    "agentLength" : 120,
+    "rotationDegree" : 3,
+    "agentVelocity" : 5,
+    "sensorsRotationalDistance":10,
+    "sensorSensitivity" : 5,
+    "buttonWidth" : 230
+}
