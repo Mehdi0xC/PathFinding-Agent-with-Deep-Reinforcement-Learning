5614852407b718ea4cbb4375df474bd2c79241c2
Mehdi Safaee <mehdi0xc@gmail.com>
2024-03-23 18:44:45 -0700

    Fix config.py

--- config.py
+++ config.py
@@ -1,51 +1,55 @@
-learningCoreSettings = {
+core_settings = {
     # Number of hidden layer neurons (For three inputs, 8 to 16 neurons work like charm)
-    "nNeurons" : 10,
+    "n_neurons": 10,
     # Discount factor (Somewhere between 0.8 and 0.9 is ok)
-    "gamma" : 0.9,
+    "gamma": 0.9,
     # Replay memory capacity (10000 is more than enough)
-    "memoryCapacity" : 10000,
+    "memory_capacity": 10000,
     # Learning-rate (Somewhere between 0.0001 to 0.005 is ok) 
-    "learningRate" : 0.001,
-    # BatchSize, number of samples taken from replay memory in each Learning Iteration
-    "batchSize" : 25,
+    "learning_rate": 0.001,
+    # Batch size, number of samples taken from replay memory in each learning iteration
+    "batch_size": 25,
     # Non-linear activation function for neurons, ReLU is used in this project but you may implement others 
-    "activationFunction" :"relu",
-    # Number of outputs, can be set to 3 (Its not generic yet)
-    "nOutputs" : 3,
-    # Number of inputs, can be set to 3 or 7 (Its not generic yet)
-    "nInputs" : 3,
-    # Regularization factor, for now its just implemented in manual design
-    "reg" : 0,  
-    # AI Backend, can be set to manual, pytorch or UART
-    "backend" : "manual",
+    "activation_function": "relu",
+    # Number of outputs, can be set to 3 (It's not generic yet)
+    "n_outputs": 3,
+    # Number of inputs, can be set to 3 or 7 (It's not generic yet)
+    "n_inputs": 3,
+    # Regularization factor, for now it's just implemented in manual design
+    "reg": 0,  
+    # AI backend, can be set to manual, pytorch or UART
+    "backend": "manual",
     # Amount of given reward for DQN algorithm
-    "rewardAmount" : 0.1,
+    "reward_amount": 0.1,
     # Punishment = amount of given negative reward for DQN algorithm
-    "punishAmount" : -1,
+    "punish_amount": -1,
     # Softmax temperature, used in softmax function implementation
-    "softmaxTemperature" : 10,
+    "softmax_temperature": 10,
     # Number of iterations in learning phase
-    "learningIterations" : 2500,
+    "learning_iterations": 2500,
     # Number of iterations in prediction phase
-    "predictionIterations" : 2500}
+    "prediction_iterations": 2500
+}
 
-learningDeviceSettings = {
-        # Device port name, if using UART as backend
-        "devicePortName" : "COM5",
-        # UART connection baudrate
-        "baudrate" : 115200
-    }
-
-environmentSettings = {    
-    "sensorSize" : 15,
-    "agentWidth" : 96,
-    "agentLength" : 120,
-    "rotationDegree" : 3,
-    "agentVelocity" : 5,
-    "sensorsRotationalDistance":15,
-    "sensorSensitivity" : 8,
-    "buttonWidth" : 230,
-    "environmentWidth" : 800,
-    "environmentHeight" : 600
+environment_settings = {
+    # Size of the robot sensors 
+    "sensor_size": 15,
+    # Width of the robot
+    "agent_width": 96,
+    # Length of the robot
+    "agent_length": 120,
+    # Degree of rotation for each rotation action
+    "rotation_degree": 3,
+    # Velocity of the robot in the environment
+    "agent_velocity": 5,
+    # Degree between each sensor
+    "sensors_rotational_distance": 15,
+    # Sensor sensitivity
+    "sensor_sensitivity": 8,
+    # Width of the button in the simulation
+    "button_width": 230,
+    # Width of the simulation environment
+    "environment_width": 800,
+    # Height of the simulation environment
+    "environment_height": 600
 }
